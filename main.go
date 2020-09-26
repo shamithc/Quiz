8e85a4df38b48ccc73ca9f9ec8d4072afd01aeb4
shamith <shamith@hatio.in>
2020-09-26 15:38:48 +0530

    Loop through all problems in CSV, collect user input and validate with answer

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	csvFileName := flag.String("csv", "problems.csv", "a csv file in the format of `questions and answer`")
+	flag.Parse()
+
+	file, err := os.Open(*csvFileName)
+
+	if err != nil {
+		exit(fmt.Sprintf("Failed to Open csv file %s\n", *csvFileName))
+	}
+
+	r := csv.NewReader(file)
+
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parese CSV file")
+	}
+
+	problems := parseLines(lines)
+
+	correct := 0
+
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d\n", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
